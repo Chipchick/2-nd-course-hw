495945bea7fc0cd8c9b08c9f75ab1a09d22b608c
Katy <shch.ev.31@gmail.com>
2023-09-06 18:35:19 +0300

    HW2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Задание 1
-let a = 10;
+/*let a = 10;
 let a = 20;
 alert(a)
 
@@ -14,33 +14,33 @@ alert(userName)
 //Задание 4
 let a = 10;
 let b = 2;
-alert(a+b)
-alert(a-b)
-alert(a*b)
-alert(a/b)
+alert(a + b)
+alert(a - b)
+alert(a * b)
+alert(a / b)
 
 //Задание 5
-let result = 2**5;
+let result = 2 ** 5;
 alert(result)
 
 //Задание 6
 let a = 9;
 let b = 2;
-alert(a % b)
+alert(a % b)*/
 
 //Задание 7
-let num=1
-num +=5;
-num -=3;
-num *=7;
-num /=3;
-num +=1;
-num -=1;
-alert (num)
+let num = 1
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num)
 
 //Задание 8
 let age = prompt('Сколько вам лет');
-alert (age)
+alert(age)
 
 //Задание 9
 const user = {
